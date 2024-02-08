b2b46b93a5fe5ac812af20d12b156041194fd011
Michael Williams <mbusiness3907@gmail.com>
2024-02-07 19:13:01 -0600

    completed & tested dataset class

--- data/ImageDataset.py
+++ data/ImageDataset.py
@@ -1,15 +1,19 @@
 import os
-from pathlib import Path
+import torch
 import pandas as pd
+from pathlib import Path
 from PIL import Image
 from torch.utils.data import Dataset
 
 
+
 class ImageDataset(Dataset):
     def __init__(self, data_path, label_path, transforms):
+        # ray.init()
         if os.path.exists(data_path) and os.path.exists(label_path):
             self.dataset_dir = Path(data_path)
             self.labels_dir = Path(label_path)
+            self.labels_df = pd.read_csv(self.labels_dir)
             self.all_filenames = os.listdir(self.dataset_dir)
         else:
             self.dataset_dir = None
@@ -22,11 +26,15 @@ class ImageDataset(Dataset):
         return len(self.all_filenames)
 
     def __getitem__(self, idx):
-        image_name = self.all_filenames[idx]
-        image_path = os.path.join(self.dataset_dir, image_name)
-        image_pil = Image.open(image_path).convert('RGB')
-        sample = self.transforms(image_pil)
+        img_name = self.all_filenames[idx]
 
+        img_path = os.path.join(self.dataset_dir, img_name)
+        img_pil = Image.open(img_path).convert('RGB')
+        # img_pil.show()
+        img_sample = self.transforms(img_pil)
 
+        img_labels_entry = (self.labels_df.loc[self.labels_df["Image Index"] == img_name])
+        img_labels_tensor = torch.tensor(img_labels_entry.iloc[:, 2:16].values.tolist()[0])
+        # print(img_labels_tensor)
 
-        return sample, image_name
+        return img_sample, img_labels_tensor
